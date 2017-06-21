0a4344c755687a678dabd48ea36836afa06c191e
Felipe Costa <felipeac@protonmail.com>
2017-06-21 15:35:05 -0300

    Organization

--- main.go
+++ main.go
@@ -8,21 +8,31 @@ import (
 	"syscall"
 )
 
+var (
+	discordKey = os.Getenv("DISCORD_TOKEN")
+)
+
 func main() {
-	discord, err := discordgo.New(os.Getenv("DISCORD_TOKEN"))
+	if discordKey == "" {
+		fmt.Println("You need an API key")
+		return
+	}
+
+	discord, err := discordgo.New(discordKey)
+
 	if err != nil {
 		fmt.Println("Fuck didn't work")
 		return
 	}
+
 	discord.AddHandler(ready)
-	fmt.Println("Hello")
 
 	err = discord.Open()
 	if err != nil {
 		fmt.Println("Didn't work again, shit: ", err)
 	}
 
-	fmt.Println("Bot's now running")
+	fmt.Println("Bot's now running, press CTRL-C to close.")
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
